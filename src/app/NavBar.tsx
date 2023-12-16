dd59bac3a407990cfdd44499e2c080759614c6af
Roman <coldshein@gmail.com>
2023-12-16 20:38:34 +0200

    build a new issue page

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -15,7 +15,7 @@ const NavBar = () => {
     <nav className='flex space-x-6 p-5 items-center border-b'>
         <Link href="/"><DiCssdeck  /></Link>
         <ul className='flex space-x-6'>
-           {links.map((link) => <li><Link href={link.href} className={classNames({
+           {links.map((link) => <li key={link.label}><Link href={link.href} className={classNames({
             'text-zinc-900': link.href === currentPath,
             'text-zinc-500': link.href !== currentPath,
             'hover:text-zinc-800 transition-colors': true
--- src/app/issues/page.tsx
+++ src/app/issues/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 import React from 'react'
 
 const IssuesPage = () => {
   return (
-    <div>IssuesPage</div>
+    <div>
+      <Link href='/issues/new'>New Issue</Link>
+    </div>
   )
 }
 
